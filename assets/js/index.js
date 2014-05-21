6ccebfe5bef59189d5a7edb05bce8caf89a13f79
Jonathan Lacefield <jlacefie@gmail.com>
2014-05-21 14:47:09 -0400

    integration success
    updating test and index to reflect the integrated runschematic1();
    function

--- assets/js/index.js
+++ assets/js/index.js
@@ -778,6 +778,22 @@ var Index = function () {
     };
     
     
+    // function to initiate schematic1
+    var runschematic1 = function () {
+   	   $.ajax({
+		    type:'GET',
+	    	url: 'http://208.96.49.194:3000/api/count/type/all',
+	      	dataType: 'jsonp',
+	      	success: function(data, status) {
+	      		$.each(data, function(key, value) {
+					 	$("#scehmatic1").append(value);
+				});
+	      	}, 
+	      	error: function(jqXHR, textStatus, errorThrown) {
+	      		alert("Error: " + errorThrown); 
+	      	}
+   	   });
+    };
     
     //end admin aditions
     return {
@@ -791,7 +807,7 @@ var Index = function () {
             //runChart2();
             runChart3(); //room analysis
             runChart4(); // slider moving chart
-            //  runschematic1();
+            runschematic1();
            // runSparkline();
            // runEasyPieChart();
         }
--- assets/js/test.js
+++ assets/js/test.js
@@ -1,31 +1,26 @@
-
 var Test = function () {
 	// function to initiate EasyPieChart
     // function to initiate schematic1
-    var runschematic1 = function () {
-    	$.ajax({
-	        type: 'GET',
-	        url: 'http://208.96.49.194:3000/api/count/type/all',
-	        dataType: 'jsonp',
-            success: function(text){
-	        	//var test = '{"0":10}';
-	        	//json = JSON.parse(test);
-	        	alert("json.message");
-	        	//alert(data);
-	        	//$("#scehmatic1").append(data["0"]);
-	        },
-	        error: function(jqXHR,error, errorThrown){
-	       // 	alert(jqXHR.responseText);
-	      //  	alert(errorThrown);
-	     //   	alert(error);
-	        }
-	    });
-	};
-    
-    //end admin aditions
+   var runschematic1 = function () {
+   	   $.ajax({
+		    type:'GET',
+	    	url: 'http://208.96.49.194:3000/api/count/type/all',
+	      	dataType: 'jsonp',
+	      	success: function(data, status) {
+	      		$.each(data, function(key, value) {
+					 	$("#scehmatic1").append(value);
+				});
+	      	}, 
+	      	error: function(jqXHR, textStatus, errorThrown) {
+	      		alert("Error: " + errorThrown); 
+	      	}
+   	   });
+    };
+      
+	//end admin aditions
      return {
         init: function () {
-            runschematic1();
+        	runschematic1();
         }
     };
 }();
